6e020423a28bf8a8aef3e7055a58b8bdfd57f994
JeruApex <Apex2673@gmail.com>
2022-02-21 08:34:23 -0600

    * Stopped from Crashing

--- app/src/main/java/com/example/kiddiecash2/DataFragment.kt
+++ app/src/main/java/com/example/kiddiecash2/DataFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 
 class DataFragment : Fragment() {
@@ -14,7 +16,14 @@ class DataFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data, container, false)
+        val view = inflater.inflate(R.layout.fragment_data, container, false)
+//        val button = view.findViewById<Button>(R.id.button)
+//        What is homefragment2... why is there a 2?
+
+//        button.setOnClickListener{
+//            findNavController().navigate(R.id.action_dataFragment_to_homeFragment2)
+//        }
+        return view
     }
 
 }
